f901fa0b32a44ca3fb82ab63eb874863764412ac
EntrySpartan <illut.polomaridarrel.09062003@gmail.com>
2026-08-03 02:38:56 +0800

    new addition as of 2:38pm, 3/2/2026

--- script.js
+++ script.js
@@ -51,3 +51,20 @@ if (footer) {
 
   footer.innerHTML = `© ${year} Polo Mari Darrel Illut`;
 }
+// ===== Back to Top Button =====
+const backToTop = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    backToTop.classList.add("show");
+  } else {
+    backToTop.classList.remove("show");
+  }
+});
+
+backToTop.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
